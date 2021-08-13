b5a3f4524119f1ab78643482c6502e5f4bf44be4
Yota002 <moscowrus2013@gmail.com>
2021-08-14 02:22:37 +0500

    another path fix

--- js/lang.js
+++ js/lang.js
@@ -2,12 +2,10 @@ let file = new XMLHttpRequest;
 let lang = navigator.language.split("-")[0];
 switch (lang) {
     case "ru":
-        // file.open("GET", "../pages/ru.html", true);
-        file.open("GET", "/portfolio/pages/ru.html", true);
+        file.open("GET", "/pages/ru.html", true);
         break;
     default:
-        // file.open("GET", "../pages/en.html", true);
-        file.open("GET", "/portfolio/pages/en.html", true);
+        file.open("GET", "/pages/en.html", true);
         break;
 }
 file.onload = function () {
